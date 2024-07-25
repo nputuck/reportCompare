fdbb9897242fbe10172d59563934c314f4652af4
DESKTOP-2C7R2M2\admin <nputuck@163>
2024-07-25 10:41:05 +0800

    change for report tools

---
+++ src/main/java/org/chinaSoft/reportCompare.java
@@ -0,0 +1,11 @@
+package org.chinaSoft;
+
+/**
+ * Hello world!
+ */
+public class reportCompare {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
